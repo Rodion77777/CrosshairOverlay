ca3173a9fa2ec46a2b810947f982ca062e91c084
Rodion <rodionchik17@gmail.com>
2025-05-14 23:23:55 +0300

    Создан класс с лимитными значениями

---
+++ CrosshairOverlay/Limits.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrosshairOverlay
+{
+    internal class Limits
+    {
+        public static int radius;
+    }
+}
