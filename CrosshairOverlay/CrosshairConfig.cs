ec7b48c1d765919bce398058d6a580b26e781a4f
Rodion <rodionchik17@gmail.com>
2025-05-19 14:19:53 +0300

    Добавлены функции чтения параметров Color filter с config.json

--- CrosshairOverlay/CrosshairConfig.cs
+++ CrosshairOverlay/CrosshairConfig.cs
@@ -17,6 +17,9 @@ public class CrosshairConfig
     public double UnrestrictedOpacity { get; set; } = 1;
     public int UnrestrictedOffsetX { get; set; } = 0;
     public int UnrestrictedOffsetY { get; set; } = 0;
+    public int FilterSize { get; set; } = 0;
+    public string FilterColor { get; set; } = "#FFFFFF";
+    public double FilterOpacity { get; set; } = 0.1;
     public bool IsCounterStrafeEnabled { get; set; } = false;
     public int csPressureDuration { get; set; } = 100;
 }
